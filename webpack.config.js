ad5168ca6d3b68ae239246c004106c5a151f94f0
Marigrace Leeman <leemuly@gmail.com>
2019-09-16 18:12:34 -0400

    project init

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  entry: './client/index.js',
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+    ],
+  },
+}
